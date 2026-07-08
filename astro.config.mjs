722f66e875bb2cc606bb85af8bbcd6faa4a38822
Kshitiz kumar <avi052001@gmail.com>
2026-07-08 22:16:46 +0530

    ga4 and gcs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,21 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kshitizkumar.vercel.app',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push", "gtag"],
+      },
+    }),
+    sitemap(),
+  ],
 });
